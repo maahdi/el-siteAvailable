63f7c6ca080cb9804e765ce85ada0e17de6662bc
yoshi <josien-lionel@hotmail.fr>
2014-03-22 17:02:35 +0100

    test requete visite

--- structureBundle/Classes/GestionMenu.php
+++ structureBundle/Classes/GestionMenu.php
@@ -79,7 +79,7 @@ class GestionMenu
         {
             $visite['total'] = $r->nb;
         }
-        $sql = 'select count(idVisite) as nb from visites as v left join utilisateur as u on v.idUser = u.idUser where extract( month from current_date) = extract( month from dateConnexion) v.idUser = 0';
+        $sql = 'select count(idVisite) as nb from visites as v left join utilisateur as u on v.idUser = u.idUser where extract( month from current_date) = extract( month from dateConnexion) and v.idUser = 0';
         $result = $db->query($sql);
         $result->setFetchMode(\PDO::FETCH_OBJ);
         foreach ($result as $r)
